7f7558c50495364cd626ddbdc72801854be1d410
liush79 <liush79@gmail.com>
2014-02-02 15:50:28 +0000

    ISSUE-5 Added some python codes for merging image function.

---
+++ downloader/merge_image.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys
+import Image
+
+def merge_image(output, path_list):
+	width = 0
+	height = 0	
+	
+	try:
+		# get total image size
+		for path in path_list:
+			img = Image.open(path)
+			if width < img.size[0]:
+				width = img.size[0]				
+			height += img.size[1]
+			
+		new_img = Image.new("RGB", (width, height), "white")
+		height = 0
+		
+		for path in path_list:
+			img = Image.open(path)
+			new_img.paste(img, (0, height))
+			height += img.size[1]
+			
+		new_img.save(output, "JPEG")
+		
+	except Exception, e:
+		print "ERROR: " + str(e)
+
+if __name__ == '__main__':	
+	list = []
+	for argv in sys.argv[1:]:
+		list.append(argv)
+	merge_image(list[0], list[1:])
+
